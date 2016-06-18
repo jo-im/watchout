87df2ac476b262954ec6f57101bdfd24a7644d63
jo-im <joann_im@hotmail.com>
2016-06-17 19:52:00 -0700

    Implemented collision function

--- client/watchout.js
+++ client/watchout.js
@@ -5,26 +5,27 @@ var h = 800;
 
 svg.attr('width', w).attr('height', h);
 
+var dragstarted = function () {
+  playerCircle.style('fill', 'green');
+};
+
+var dragged = function (d) {
+  d3.select(this).attr('cx', d[0] = d3.event.sourceEvent.x)
+                 .attr('cy', d[1] = d3.event.sourceEvent.y);
+};
+
+// do we need this?
+var dragended = function () {
+  d3.select(this).classed('dragging', false);
+};
+
+
 var drag = d3.behavior.drag()
-        .origin(function(d) { return d;})
+        .origin(function(d) { return d; })
         .on('dragstart', dragstarted)
         .on('drag', dragged)
         .on('dragend', dragended);
 
-
-function dragstarted () {
-  playerCircle.style('fill', 'green');
-}
-
-function dragged(d) {
-  console.log('d3.event', d3.event);
-  d3.select(this).attr('cx', d[0] = d3.event.sourceEvent.x)
-                 .attr('cy', d[1] = d3.event.sourceEvent.y);
-}
-
-function dragended() {
-  d3.select(this).classed('dragging', false);
-}
 
 var playerData = [1];
 // player circle
@@ -34,10 +35,10 @@ var playerCircle = svg.selectAll('circle')
                       .append('circle')
                       .call(drag);
 
-playerCircle.attr('r', '5px')
+playerCircle.attr('r', '10px')
     .attr('cx', function(d) { return w / 2; })
     .attr('cy', function(d) { return h / 2; })
-    .attr('id', 'player')
+    .attr('id', 'player');
     //.call(drag);
 
 
@@ -52,58 +53,53 @@ var circles = svg.selectAll('circle')
                  .attr('cy', function(d) { return h * Math.random(); })
                  .attr('class', 'enemies');
 
-var update = function(data) {
-  //data join
-  //join new data with old elements
+// var update = function(data) {
+//   //data join
+//   //join new data with old elements
 
-  //update
-  //update old elements
-  circles.attr('class', 'update')
-         .transition()
-         .duration(750)
-         .attr('cx', function(d) { return w * Math.random(); })
-         .attr('cy', function(d) { return h * Math.random(); });
+//   //update
+//   //update old elements
+//   circles.attr('class', 'update')
+//          .attr('class', 'enemies')
+//          .transition()
+//          .duration(750)
+//          .attr('cx', function(d) { return w * Math.random(); })
+//          .attr('cy', function(d) { return h * Math.random(); });
+// };
+
+
+
+//setInterval(function() { update(circles); }, 1500);
+
+
+
+
+
+// collision function
+
+var collision = function() {
+  var player = d3.selectAll('circle')[0][0];
+  var enemies = d3.selectAll('circle')[0].slice(1);
+
+  for (var i = 0; i < enemies.length; i++) {
+    //console.log('enemies[i].attributes.cx.value is', enemies[i].attributes.cx.value);
+    // if (Math.round(enemies[i].attributes.cx.value) === Math.round(player.attributes.cx.value) && Math.round(enemies[i].attributes.cy.value) === Math.round(player.attributes.cy.value)) {
+    //   console.log('colliding!!!');
+    // }
+
+    if ( Math.abs(enemies[i].attributes.cx.value - player.attributes.cx.value) <= 19 && Math.abs(player.attributes.cy.value - enemies[i].attributes.cy.value) <= 19 ) {
+      console.log('enemies[i] is', enemies[i]);
+      console.log('colliding!!!');
+    }
+  }
 };
 
-setInterval(function() { update(circles); }, 1500);
-
-
-// dragging method for player
-
-
+setInterval(function() {
+  collision();
+}, 100);
 
 
 
 
 
-
-
-// data set for our enemies
-// var enemiesData = d3.range(5).map(function() {
-//   var x = Math.random() * w;
-//   var y = Math.random() * h;
-//   return {
-//     vx: Math.random() * 2 - 1,
-//     vy: Math.random() * 2 - 1,
-//     path: d3.range(12).map(function() { return [x, y]; })
-//   }
-// })
-
-// // creating group of elements w/n SVG
-// // var g = svg.selectAll('g')
-// //            .data(enemiesData)
-// //            .enter()
-// //            .append('g')
-// //            .attr('cx', function(d) { return d.x })
-// //            .attr('cy', function(d) { return d.y });
-
-// // appending circle to group of elements
-// var circles = svg.selectAll('circle')
-//    .data(enemiesData)
-//    .enter()
-//    .append('circle')
-//    .attr('r', '5px')
-//    .attr('class', 'enemies');
-//    .attr('cx', function(d) { return d.x })
-//    .attr('cy', function(d) { return d.y });
 
